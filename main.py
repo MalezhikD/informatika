28b8da735b952246e76b20d7a547980b8a0bec56
Олег <prostomolotov.os@edu.spbstu.ru>
2026-02-13 00:39:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 name = "Студент"
 a = 10
 b = 5
-result = a + b
+c = 11
+result = a + b + c
 print(f"Привет, {name}!")
-print(f"Результат сложения {a} + {b} равен {result}")
+
+print(f"Результат сложения {a} + {b} + {c} равен {result}")
